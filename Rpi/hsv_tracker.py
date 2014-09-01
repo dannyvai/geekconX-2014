77d391b687de523aa4d348e6951b72c14a4103c5
Danny Wainshtein <danny.wainshtein@gmail.com>
2014-09-02 00:23:51 +0300

    tracker++

--- Rpi/hsv_tracker.py
+++ Rpi/hsv_tracker.py
@@ -14,35 +14,69 @@ img_height = 480
 
 def drawObject(img,x,y):
 	global img_width,img_height
-
-	cv2.circle(img,(x,y),20,(0,255,0),3)
+	print img
+	print x
+	print y
+	cv2.circle(img,(x,y),20,(0,0,255),3)
 	if(y-25>0):
-		cv2.line(img,(x,y),(x,y-25),(0,255,0),2)
+		cv2.line(img,(x,y),(x,y-25),(0,0,255),2)
 	else:
-		cv2.line(img,(x,y),(x,0),(0,255,0),2)
+		cv2.line(img,(x,y),(x,0),(0,0,255),2)
 
 	if(y+25<img_height):
-		cv2.line(img,(x,y),(x,y+25),(0,255,0),2)
+		cv2.line(img,(x,y),(x,y+25),(0,0,255),2)
 	else:
-		cv2.line(img,(x,y),(x,img_height),(0,255,0),2)
+		cv2.line(img,(x,y),(x,img_height),(0,0,255),2)
 
 	if(x-25>0):
-		cv2.line(img,(x,y),(x-25,y),(0,255,0),2)
+		cv2.line(img,(x,y),(x-25,y),(0,0,255),2)
 	else:
-		cv2.line(img,(x,y),(0,y),(0,255,0),2)
+		cv2.line(img,(x,y),(0,y),(0,0,255),2)
 
 	if(x+25 < img_width):
-		cv2.line(img,(x,y),(x+25,y),(0,255,0),2)
+		cv2.line(img,(x,y),(x+25,y),(0,0,255),2)
 	else:
-		cv2.line(img,(x,y),(img_width,y),(0,255,0),2)
+		cv2.line(img,(x,y),(img_width,y),(0,0,255),2)
 
 	font = cv2.FONT_HERSHEY_SIMPLEX
-	cv2.putText(img,str(x)+','+str(y),(x,y+30),font,1,(0,255,0),2)
+	cv2.putText(img,str(x)+','+str(y),(x,y+30),font,1,(0,0,255),4)
 
+def trackFilteredObject(threshold,img):
+	temp = threshold
+	refArea = 0
+	objectFound = False
+	contours,hierarchy = cv2.findContours(temp,cv2.cv.CV_RETR_CCOMP,cv2.cv.CV_CHAIN_APPROX_SIMPLE)
+	if len(contours) > 0 and len(contours) < 100:
+		try:
+			for idx in range(0,len(hierarchy[0])):
+				m = cv2.moments(contours[idx])
+				x,y,w,h = cv2.boundingRect(contours[idx])
+				detect_area = w*h # cv2.contourArea(contours[idx])
+				#cv2.drawContours(img,contours[idx],-1,(0,0,255),5)
+				#print detect_area
+				if detect_area > 100:	
+					
+					cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+					#print detect_area 
+					objectFound = True
+					refArea = detect_area
+					track_x = x
+					track_y = y
+					#print track_x,track_y
+					#drawObject(img,track_x,track_y)
+		except:
+			import pdb;pdb.set_trace()
+
+	else:
+		objectFound = False
+
+	#if objectFound is True:
+	#	drawObject(img,track_x,track_y)
+				
 #Sliders variables
-h_min_val,h_max_val = 0,256
-s_min_val,s_max_val = 0,256
-v_min_val,v_max_val= 0,256
+h_min_val,h_max_val = 0,0
+s_min_val,s_max_val = 255,255
+v_min_val,v_max_val= 0,0
 h_min_slide = 0
 h_max_slide = 0
 s_min_slide = 0
@@ -54,22 +88,22 @@ def create_sliders():
 	global h_min_slide,s_min_slide,v_min_slide,h_max_slide,s_max_slide,v_max_slide
 	H_min = plt.axes([0.25, 0.1, 0.65, 0.03])
 	H_max = plt.axes([0.25, 0.15, 0.65, 0.03])
-	h_min_slide = Slider(H_min, 'H min', 1, 256)
-	h_max_slide = Slider(H_max,'H max', 1, 256)
+	h_min_slide = Slider(H_min, 'H min', 0, 180)
+	h_max_slide = Slider(H_max,'H max', 0, 180)
 	h_min_slide.on_changed(update)
 	h_max_slide.on_changed(update)
 
 	V_min = plt.axes([0.25, 0.20, 0.65, 0.03])
 	V_max = plt.axes([0.25, 0.25, 0.65, 0.03])
-	v_min_slide = Slider(V_min, 'V min', 1, 256)
-	v_max_slide = Slider(V_max, 'V max', 1, 256)
+	v_min_slide = Slider(V_min, 'V min', 0, 255)
+	v_max_slide = Slider(V_max, 'V max', 0, 255)
 	v_min_slide .on_changed(update)
 	v_max_slide .on_changed(update)
 
 	S_min  = plt.axes([0.25, 0.30, 0.65, 0.03])
 	S_max = plt.axes([0.25, 0.35, 0.65, 0.03])
-	s_min_slide = Slider(S_min, 'S min', 1, 256)
-	s_max_slide = Slider(S_max, 'S max', 1, 256)
+	s_min_slide = Slider(S_min, 'S min', 0, 255)
+	s_max_slide = Slider(S_max, 'S max', 0, 255)
 	s_min_slide .on_changed(update)
 	s_max_slide .on_changed(update)
 	
@@ -80,8 +114,8 @@ def update(val):
     global h__min_val,s_min_val,v_min_val,h_max_val,s_max_val,v_max_val
     h_min_val = int(h_min_slide.val)
     h_max_val = int(h_max_slide.val)
-    s_minval = int(s_min_slide.val)
-    s_maxval = int(s_max_slide.val)
+    s_min_val = int(s_min_slide.val)
+    s_max_val = int(s_max_slide.val)
     v_min_val = int(v_min_slide.val)
     v_max_val = int(v_max_slide.val)
 
@@ -95,9 +129,11 @@ def morph(threshold):
 	erode_ker = np.ones((3,3),np.uint8)
 	dilate_ker = np.ones((8,8),np.uint8)
 
-	erosion = cv2.erode(threshold,erode_ker,iterations = 2)
+	erosion = cv2.erode(threshold,erode_ker,iterations = 1)
 	dilation = cv2.dilate(erosion,dilate_ker,iterations = 1)
 	return dilation
+
+s_min_val,s_max_val = 255,255
 
 
 while True:
@@ -107,6 +143,11 @@ while True:
 	threshold = cv2.inRange(img_hsv,(h_min_val,s_min_val,v_min_val),(h_max_val,s_max_val,v_max_val))
 
 	threshold = morph(threshold)
-	drawObject(threshold,100,100)
-	cv2.imshow("test",threshold)
+	cv2.imshow("binary",threshold)
+	trackFilteredObject(threshold,img_rgb)
+#	drawObject(threshold,100,100)
+
+	cv2.imshow("true_image",img_rgb)
+	cv2.imshow("hsv",img_hsv)
+	#cv2.imshow("binary",threshold)
 	cv2.waitKey(1)
